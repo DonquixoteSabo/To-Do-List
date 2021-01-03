7ac738612bcc7bba864af35a3276d759b258a25c
DonquixoteSabo <mixboksu@gmail.com>
2021-01-03 13:07:40 +0100

    next update for Navigation after code review

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -4,22 +4,24 @@ import {Link} from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation({click, items, activeIndex}) {
-    const menu = items.map((item,index) => (
-        <li onClick={()=> click(index)} className={`nav__link ${index === activeIndex ? 'active' : null}`} key={index}>
-            <Link to={`${item}`}>{item}</Link>
-         </li>
-    ))
+
+    const checkClasses = (index) => (
+        `nav__link ${index === activeIndex ? 'active': null}`
+    )
+
     return (
         <nav className='nav'>
             <ul className="nav__list">
-             {menu}
+             {items.map((item,index) => (
+        <li onClick={()=> click(index)} className={checkClasses(index)} key={index}>
+            <Link to={item}>{item}</Link>
+         </li>
+    ))}
             </ul>
         </nav>
     )
 }
 
 export default Navigation;
-/* <li className='nav__link'><Link to='/'>All</Link></li>
-                <li className='nav__link'><Link to='/active'>Active</Link></li>
-                <li className='nav__link'><Link to='/completed'>Completed</Link></li> */
+
 
